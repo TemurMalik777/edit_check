f31f18c3fe6d70980337b472319b98b29f9e659d
Temur Malikov <temur@example.com>
2025-10-17 10:02:17 +0500

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-from database.chek_importer import ChekImporter
+from selenium_scripts.actions.process_excel import process_excel
+from config import DEFAULT_EXCEL_PATH, DEFAULT_ZIP_PATH
+from database.chek_importer import ChekImporter  # ✅ bu siz yozgan klass joylashgan fayl
+
+
+class LogToTerminal:
+    def append(self, text):
+        print(text)
+
 
 if __name__ == "__main__":
-    excel_path = "test_data.xlsx"  # Excel fayl nomi (aniq yo‘l bo‘lsin)
-    importer = ChekImporter()
-    importer.import_from_excel(excel_path)
+    print("Jarayon boshlandi...")
+    try:
+        log_panel = LogToTerminal()
+        process_excel(DEFAULT_EXCEL_PATH, DEFAULT_ZIP_PATH, log_panel)
+        print("✅ Excel jarayoni tugadi, endi ma’lumotlar bazaga yoziladi...")
+
+        importer = ChekImporter()
+        importer.import_from_excel(DEFAULT_EXCEL_PATH)
+        print("✅ Jarayon tugadi, ma’lumotlar bazaga yozildi.")
+
+    except Exception as e:
+        print("❌ Xato:", e)
